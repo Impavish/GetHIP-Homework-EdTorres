c3ad703f33242d3d555a9e91bdfd993f89ab1314
ED <torres.eduardo.steven@gmail.com>
2015-09-28 18:01:56 -0500

    Finished PIN finder.

--- Assignment1.java
+++ Assignment1.java
@@ -1,1 +1,22 @@
-public class Assignment1
+import java.util.Scanner;
+
+public class Assignment1 {
+	public static void main(String[] args) {
+		//Check for PIN
+			Scanner input = new Scanner(System.in);
+				int pin;									
+				System.out.print("Please enter your pin:");
+				pin = input.nextInt();
+					if (pin == 7885){
+					System.out.print("Welcome to HugeBank!");
+							}//bruh	
+							else while(pin != 7885) 
+							{
+								System.out.print("Invalid PIN! Please try again.");
+								pin = input.nextInt();
+				}
+			}//the
+						
+		}
+
+				
