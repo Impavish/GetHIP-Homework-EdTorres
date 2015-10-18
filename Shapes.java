056a896b252216ab19663c8a65d260410f0cedce
ED <torres.eduardo.steven@gmail.com>
2015-10-17 21:13:04 -0500

    Updated

---
+++ Shapes.java
@@ -0,0 +1,26 @@
+public abstract class Shapes implements printable {
+	
+	//Shape variables, for our purposes there's no difference in the following variables.
+	
+	private string shapeType();
+	private string shapeName();
+
+	//ShapeType
+
+	public void String getshapeType() {
+		return shapeType;
+		}
+	public void String setshapeType() {
+		this.shapeType = shapeType;
+		}
+	//ShapeName
+	
+	public void String getshapeName() {
+		return shapeName;
+		}
+	public void String setshapeName() {
+		this.shapeName = shapeName;
+		}
+	public abstract getArea();
+
+	}
